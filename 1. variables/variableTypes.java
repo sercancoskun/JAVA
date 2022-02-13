fdf6474253032b03bfe63d64304d95c79ff53d15
sercancoskun <99555577+sercancoskun@users.noreply.github.com>
2022-02-13 01:57:17 -0600

    variables

---
+++ 1. variables/variableTypes.java
@@ -0,0 +1,19 @@
+public class VariableTypes{
+	
+    static int a=1;//static variable
+	
+    void method(){
+		
+        int b=2;//local variable
+		
+    }
+    public static void main(String args[]){
+        
+        int c=5;//instance variable
+
+        System.out.println(a);
+        //System.out.println(b);
+        System.out.println(c);
+    }
+	
+}//end of class
---
+++ 1. variables/variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int numOfStudents = 12;
+
+        System.out.println("Number of Students :" + numOfStudents);
+
+    }
+
+}
