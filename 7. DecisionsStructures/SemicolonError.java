38c3601c76c601353d34741936405f102ccf125b
sercancoskun <99555577+sercancoskun@users.noreply.github.com>
2022-02-26 14:17:56 -0600

    Create SemicolonError.java

---
+++ 7. DecisionsStructures/SemicolonError.java
@@ -0,0 +1,10 @@
+public class SemicolonError{
+        public static void main(String[] args){
+
+        int x = 0, y = 10;
+
+        if (x>y);
+            System.out.println(x + " is greater than " + y);
+
+    }
+}
