36fbb9aa69dfa6ddea323defcb7ae134014c7c78
sercancoskun <99555577+sercancoskun@users.noreply.github.com>
2022-02-26 18:35:02 -0600

    Update SemicolonError.java

--- 7. DecisionsStructures/SemicolonError.java
+++ 7. DecisionsStructures/SemicolonError.java
@@ -1,10 +1,13 @@
 public class SemicolonError{
-        public static void main(String[] args){
+    public static void main(String[] args){
 
         int x = 0, y = 10;
 
+        //DO NOT prematurely terminate an if statement with a semicolon
+        //this will not display any error message; however, the condition will have a null statement
+        //it will not execute anything
         if (x>y);
-            System.out.println(x + " is greater than " + y);
+            System.out.println(x + " is greater than " + y); //this will always execute
 
     }
 }
