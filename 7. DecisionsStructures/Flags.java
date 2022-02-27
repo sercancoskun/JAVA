3af1d3eafa4e6894777bf2dbfb10e7b93cb7c566
sercancoskun <99555577+sercancoskun@users.noreply.github.com>
2022-02-26 19:52:27 -0600

    Update
    New Files:
    - ComparingChars.java
    - Flags.java

---
+++ 7. DecisionsStructures/Flags.java
@@ -0,0 +1,14 @@
+public class Flags{
+    public static void main(String[] args){
+
+        boolean highScore = false;
+        int score = 98;
+
+        if(score > 95)
+            highScore = true;
+
+        if (highScore);
+            System.out.println("That's a high score!");
+
+    }
+}
