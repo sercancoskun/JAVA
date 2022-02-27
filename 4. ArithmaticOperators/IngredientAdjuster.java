c55d911723a326547c27dddf992c1e023b77ea58
sercancoskun <99555577+sercancoskun@users.noreply.github.com>
2022-02-26 22:20:40 -0600

    Update

---
+++ 4. ArithmaticOperators/IngredientAdjuster.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+//This program demonstrates a nested if statement
+public class IngredientAdjuster{
+
+	public static void main (String[] args){
+		
+		double sugarNeeded = 1.5 / 48; // Cups of sugar needed to make 48 cookies
+		double butterNeeded = 1.0 / 48; // Cups of butter needed to make 48 cookies
+		double flourNeeded = 2.75 / 48; // Cups of flour needed to make 48 cookies
+		
+		// Create a Scanner object for keyboard input
+		Scanner keyboard = new Scanner (System.in);
+		//Get the number of cookies wanted to made
+		System.out.print("How many cookies do you want to make? : ");
+		double cookie = keyboard.nextDouble();
+		
+		//Give the ingredient
+		System.out.println("You need: ");
+		System.out.println(cookie * sugarNeeded + " cups of sugar");
+		System.out.println(cookie * butterNeeded + " cups of butter");
+		System.out.println(cookie * flourNeeded + " cups of flour");
+
+	}
+}
