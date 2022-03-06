851d99dc1c826b19ffae1a57ebd7dd10f1e0f42c
sercancoskun <99555577+sercancoskun@users.noreply.github.com>
2022-03-05 21:31:59 -0600

    Update SwitchNumber.java

--- 7. DecisionsStructures/SwitchNumber.java
+++ 7. DecisionsStructures/SwitchNumber.java
@@ -3,32 +3,26 @@ public class SwitchNumber{
 
     public static void main(String args[]){
 
-	Scanner keyboard = new Scanner(System.in);
-	System.out.println("Please type a number from the list (1-4):  ");
-    int number = keyboard.nextInt();
-	
-	switch(number){
-		
-		case 1:
-			System.out.println("1");
-			break;
-		case 2:
-			System.out.println("2");
-			break;
-		case 3:
-			System.out.println("3");
-			break;
-		case 4:
-			System.out.println("4");
-			break;
-		default:
-		System.out.println("Please use only 1,2,3 or 4!");
-		
-		
-	}
-        
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please type a number from the list (1-4): ");
+		int number = keyboard.nextInt();
+
+		switch(number){
+			case 1:
+				System.out.println("1");
+				break;
+			case 2:
+				System.out.println("2");
+				break;
+			case 3:
+				System.out.println("3");
+				break;
+			case 4:
+				System.out.println("4");
+				break;
+			default:
+			System.out.println("Please use only 1,2,3 or 4!");
+
+		}
     }
 }
-
-
-
